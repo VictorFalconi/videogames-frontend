b6959495809838b7a6f9e14b2cbc54dba473d0e8
Victor Falconi <v.falconi.m@gmail.com>
2022-11-09 13:37:38 -0500

    HTTPS database url

--- src/redux/actions.js
+++ src/redux/actions.js
@@ -1,10 +1,10 @@
 import { ALL_VIDEOGAMES, ALL_GENRES, ALL_PLATFORMS, GAME_BY_NAME, GAME_BY_ID, CREATE_GAME, CLEAR_DETAIL, 
-    CLEAR_SEARCH, CLEAR_FILTER, FILTER_BY_ORIGIN, FILTER_BY_GENRE, ORDER_BY_NAME, ORDER_BY_RATING, FILTER_BY_PLATFORMS } from "./actionTypes";
+    CLEAR_SEARCH, CLEAR_FILTER, FILTER_BY_ORIGIN, FILTER_BY_GENRE, ORDER_BY_NAME, ORDER_BY_RATING, FILTER_BY_PLATFORMS } from "./actio3001nTypes";
 
 
 export function getAllVideogames () {
     return function (dispatch) {
-        return fetch ('http://videogames-backend.vercel.app/videogames')
+        return fetch ('https://videogames-backend.vercel.app/videogames')
         .then (response => response.json())
         .then (videogames => dispatch({type: ALL_VIDEOGAMES, payload: videogames}))
         .catch (error => alert ('Sorry, I cant get all videogames, please reload', error.message));
@@ -13,7 +13,7 @@ export function getAllVideogames () {
 
 export function getGameName (name) {
     return function (dispatch) {
-        return fetch (`http://videogames-backend.vercel.app/videogames?name=${name}`)
+        return fetch (`https://videogames-backend.vercel.app/videogames?name=${name}`)
         .then (response => response.json())
         .then (game => dispatch({type: GAME_BY_NAME, payload: game}))
         .catch (error => dispatch({ type: GAME_BY_NAME, payload: error.message='Error' }));
@@ -22,7 +22,7 @@ export function getGameName (name) {
 
 export function getGameId (id) {
     return function (dispatch) {
-        return fetch (`http://videogames-backend.vercel.app/videogames/${id}`)
+        return fetch (`https://videogames-backend.vercel.app/videogames/${id}`)
         .then (response => response.json())
         .then (gameId => dispatch({type: GAME_BY_ID, payload: gameId}))
         .catch (error => dispatch({ type: GAME_BY_ID, payload: error.message='Error' }));
@@ -31,7 +31,7 @@ export function getGameId (id) {
 
 export function postNewGame (payload) {
     return function (dispatch) {
-        return fetch ('http://videogames-backend.vercel.app/videogames', {
+        return fetch ('https://videogames-backend.vercel.app/videogames', {
             method: 'POST',
             body: JSON.stringify(payload),
             headers: {'Content-type': 'application/json; charset=UTF-8'}
@@ -44,7 +44,7 @@ export function postNewGame (payload) {
 
 export function getAllGenres () {
     return function (dispatch) {
-        return fetch ('http://videogames-backend.vercel.app/genres')
+        return fetch ('https://videogames-backend.vercel.app/genres')
         .then (response => response.json())
         .then (genres => dispatch({type: ALL_GENRES, payload: genres}))
         .catch (error => console.log (error));
@@ -53,7 +53,7 @@ export function getAllGenres () {
 
 export function getAllPlatforms () {
     return function (dispatch) {
-        return fetch ('http://videogames-backend.vercel.app/platforms')
+        return fetch ('https://videogames-backend.vercel.app/platforms')
         .then (response => response.json())
         .then (platforms => dispatch({type: ALL_PLATFORMS, payload: platforms}))
         .catch (error => console.log (error));
